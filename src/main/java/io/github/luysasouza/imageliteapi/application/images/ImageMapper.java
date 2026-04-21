8c1aed82ff27c7b5e05c7227aacdaca97867616a
Maria Luysa de Souza <luysa.contato@gmail.com>
2026-04-21 15:38:08 -0300

    feat: adiciona ImageMapper e melhorias no upload de imagens

---
+++ src/main/java/io/github/luysasouza/imageliteapi/application/images/ImageMapper.java
@@ -0,0 +1,24 @@
+package io.github.luysasouza.imageliteapi.application.images;
+
+import io.github.luysasouza.imageliteapi.domain.entity.Image;
+import io.github.luysasouza.imageliteapi.domain.enums.ImageExtension;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ImageMapper {
+
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+        return Image.builder()
+                .name(name)
+                .tags(String.join(",", tags)) // ["tag1","tag2", ...] -> tag1,tag2
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+    }
+}
--- src/main/java/io/github/luysasouza/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/luysasouza/imageliteapi/application/images/ImagesController.java
@@ -3,6 +3,7 @@ package io.github.luysasouza.imageliteapi.application.images;
 import io.github.luysasouza.imageliteapi.domain.entity.Image;
 import io.github.luysasouza.imageliteapi.domain.enums.ImageExtension;
 import io.github.luysasouza.imageliteapi.domain.service.ImageService;
+import jakarta.servlet.Servlet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +26,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -33,16 +37,19 @@ public class ImagesController {
 
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",", tags)) // ["tag1","tag2", ...] -> tag1,tag2
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage =  service.save(image);
+        URI imageUri = buildImageURL(savedImage);
 
-        service.save(image);
+        return ResponseEntity.created(imageUri).build();
+    }
 
-        return ResponseEntity.ok().build();
+    // localhost:8080/v1/images/...
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build().toUri();
     }
 }
