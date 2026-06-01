784efe27ee411d550bcd1fa6e90821fb0c0a6fe9
Maria Luysa de Souza <luysa.contato@gmail.com>
2026-05-31 22:27:36 -0300

    feat: adiciona navegação para página de formulário

--- frontend/src/app/galeria/page.tsx
+++ frontend/src/app/galeria/page.tsx
@@ -4,6 +4,7 @@ import { Template, ImageCard } from '@/components'
 import { Image } from '@/resources/image/image.resource'
 import { useImageService } from '@/resources/image/image.service'
 import { useState } from 'react'
+import Link from 'next/link';
 
 export default function GaleriaPage() {
 
@@ -18,7 +19,7 @@ export default function GaleriaPage() {
         const result = await useService.buscar(query, extension);
         setImages(result);
         setLoading(false);
-    }   
+    }
 
     function renderImageCard(image: Image) {
         return (
@@ -42,15 +43,17 @@ export default function GaleriaPage() {
                     <input type="text"
                         onChange={event => setQuery(event.target.value)}
                         className='border px-3 py-2 rounded-lg text-gray-900' />
-                    <select onChange={event => setExtension(event.target.value)} 
-                            className='border px-4 py-2 rounded-lg text-gray-900'>
+                    <select onChange={event => setExtension(event.target.value)}
+                        className='border px-4 py-2 rounded-lg text-gray-900'>
                         <option value="">All formats</option>
                         <option value="PNG">PNG</option>
                         <option value="JPEG">JPEG</option>
                         <option value="GIF">GIF</option>
                     </select>
                     <button className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-300' onClick={searchImages}>Search</button>
-                    <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-300'>Add New</button>
+                    <Link href="/formulario">
+                        <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-300'>Add New</button>
+                    </Link>
                 </div>
             </section>
 
