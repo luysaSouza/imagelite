c3470cab0e904a0c525130d086474532c81c6e00
Maria Luysa de Souza <luysa.contato@gmail.com>
2026-06-02 22:11:17 -0300

    feat: finaliza estrutura do formulário de cadastro de imagens

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -4,12 +4,14 @@ interface ButtonProps {
     style?: string;
     label?: string;
     onClick?: (event: any) => void;
+    type?: "submit" | "button" | "reset" | undefined;
 }
 
-export const Button: React.FC<ButtonProps> = ({ style, label, onClick }: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({ style, label, onClick, type }: ButtonProps) => {
     return (
         <>
             <button className={`${style} text-white px-4 py-2 rounded-lg`}
+                type={type}
                 onClick={onClick}>
                 {label}
             </button>
